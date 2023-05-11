cd886ac2c5d02b7c75bde84ff26b56da0cbffbcf
z-fujimori <fuji.tai.29@i.softbank.jp>
2023-05-11 12:50:58 +0900

    マイグレーション途中

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
-    //
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
+    public function index(Post $post)
     {
-        return $post->get();//$postの中身を戻り値にする。
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        //return view('posts/'. $post->id);
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function update(PostRequest $request, Post $post){
+        $input_post = $request['post'];
+        $post->fill($input_post)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post){
+        return view('posts/edit')->with(['post' => $post]);
+    }
+    
+    public function delete(Post $post){
+        $post->delete();
+        return redirect('/');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Post extends Model
 {
+    use SoftDeletes;
     use HasFactory;
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+    
 }
